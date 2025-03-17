b71de3e5252a90060bc90fa053401cc5a41b6ae2
Eya Bellaaj <eyabellaaj7@gmail.com>
2025-03-17 23:25:15 +0100

    Gestion des commandes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,7 +4,7 @@ const orderController = require('../controllers/orderController');
 const checkRole = require('../middlewares/checkRole');
 
 // Protection admin/personnel_magasin
-router.use(checkRole('admin')); // ou checkRole('personnel_magasin') selon vos besoins
+router.use(checkRole('user')); // ou checkRole('personnel_magasin') selon vos besoins
 
 router.get('/', orderController.getAllOrders);
 router.get('/:id', orderController.getOrderById);
