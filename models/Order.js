373b1e79e798972f5f23d9a3d31b7958cf4486ad
ouer-fatma <fatmazahra.ouerghui@esprit.tn>
2025-04-04 01:15:50 +0100

    Gestion Commande

--- models/Order.js
+++ models/Order.js
@@ -1,7 +1,11 @@
 const { sql, poolPromise } = require('../db');
 
 class Order {
-  constructor(GP_TIERS, GP_TOTALHT, GP_TOTALTTC, GP_DATECREATION, GP_DEPOT) {
+  constructor(GP_NATUREPIECEG, GP_SOUCHE, GP_NUMERO, GP_INDICEG, GP_TIERS, GP_TOTALHT, GP_TOTALTTC, GP_DATECREATION, GP_DEPOT) {
+    this.GP_NATUREPIECEG = GP_NATUREPIECEG;
+    this.GP_SOUCHE = GP_SOUCHE;
+    this.GP_NUMERO = GP_NUMERO;
+    this.GP_INDICEG = GP_INDICEG;
     this.GP_TIERS = GP_TIERS;
     this.GP_TOTALHT = GP_TOTALHT;
     this.GP_TOTALTTC = GP_TOTALTTC;
@@ -12,14 +16,18 @@ class Order {
   async save() {
     const pool = await poolPromise;
     const result = await pool.request()
+      .input('GP_NATUREPIECEG', sql.NVarChar(3), this.GP_NATUREPIECEG)
+      .input('GP_SOUCHE', sql.NVarChar(6), this.GP_SOUCHE)
+      .input('GP_NUMERO', sql.Int, this.GP_NUMERO)
+      .input('GP_INDICEG', sql.Int, this.GP_INDICEG)
       .input('GP_TIERS', sql.NVarChar(17), this.GP_TIERS)
       .input('GP_TOTALHT', sql.Numeric(19,4), this.GP_TOTALHT)
       .input('GP_TOTALTTC', sql.Numeric(19,4), this.GP_TOTALTTC)
       .input('GP_DATECREATION', sql.DateTime, this.GP_DATECREATION)
       .input('GP_DEPOT', sql.NVarChar(6), this.GP_DEPOT)
       .query(`
-        INSERT INTO PIECE (GP_TIERS, GP_TOTALHT, GP_TOTALTTC, GP_DATECREATION, GP_DEPOT)
-        VALUES (@GP_TIERS, @GP_TOTALHT, @GP_TOTALTTC, @GP_DATECREATION, @GP_DEPOT)
+        INSERT INTO PIECE (GP_NATUREPIECEG, GP_SOUCHE, GP_NUMERO, GP_INDICEG, GP_TIERS, GP_TOTALHT, GP_TOTALTTC, GP_DATECREATION, GP_DEPOT)
+        VALUES (@GP_NATUREPIECEG, @GP_SOUCHE, @GP_NUMERO, @GP_INDICEG, @GP_TIERS, @GP_TOTALHT, @GP_TOTALTTC, @GP_DATECREATION, @GP_DEPOT)
       `);
 
     return result.rowsAffected;
@@ -27,3 +35,5 @@ class Order {
 }
 
 module.exports = Order;
+
+  
